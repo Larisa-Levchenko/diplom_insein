234eb1327881a5521b499c38c85ed4394a3a2942
Larisa-Levchenko <larisa.levchenko.98@mail.ru>
2020-09-23 21:14:25 +0700

    feat: adaptive menu

--- src/modules/toggleMenu.js
+++ src/modules/toggleMenu.js
@@ -2,13 +2,19 @@ const toggleMenu = () => {
     const menu = document.querySelector('.popup-dialog-menu');
     document.body.addEventListener('click', (event) => {
         const target = event.target;
-
-        if (target.closest('.menu__icon') !== null) {
-            menu.classList.add('active');
-        } else if (target.closest('.popup-dialog-menu') === null || target.closest('.close-menu') !== null || target.closest('.menu-link') !== null) {
-            menu.classList.remove('active');
+        if (screen.width > 576) {
+            if (target.closest('.menu__icon') !== null) {
+                menu.classList.add('active');
+            } else if (target.closest('.popup-dialog-menu') === null || target.closest('.close-menu') !== null || target.closest('.menu-link') !== null) {
+                menu.classList.remove('active');
+            }
+        } else {
+            if (target.closest('.menu__icon') !== null) {
+                menu.classList.add('active-mini');
+            } else if (target.closest('.popup-dialog-menu') === null || target.closest('.close-menu') !== null || target.closest('.menu-link') !== null) {
+                menu.classList.remove('active-mini');
+            }
         }
-
     });
 };
 
