282ad02ff38db009116e49ff0cdc473f4d837b91
Larisa-Levchenko <larisa.levchenko.98@mail.ru>
2020-09-24 12:49:06 +0700

    feat: create function phoneValidator

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@
 
 import showPhone from "./modules/showPhone.js";
 import toggleMenu from "./modules/toggleMenu.js";
+import phoneValidator from "./modules/phoneValidator.js";
 
 showPhone();
 toggleMenu();
+phoneValidator();
---
+++ src/modules/phoneValidator.js
@@ -0,0 +1,12 @@
+const phoneValidator = () => {
+    for (let i = 0; i < 6; i++) {
+        const linkPhone = document.querySelector(`#feedback-input${i+1}`);
+        linkPhone.autocomplete = "off";
+        linkPhone.pattern = "[+][7][(][0-9]{3}[)][0-9]{3}[-][0-9]{2}[-][0-9]{2}";
+        linkPhone.addEventListener('input', () => {
+            linkPhone.value = linkPhone.value.replace(/[^0-9()+-]/i, "");
+        });
+    }
+};
+
+export default phoneValidator;
