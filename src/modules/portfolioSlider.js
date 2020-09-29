30095bf9749cbc796f20f63aa2ccdec7a627b1aa
Larisa-Levchenko <larisa.levchenko.98@mail.ru>
2020-09-29 12:53:23 +0700

    feat: edit function portfilioSlider

--- src/modules/portfolioSlider.js
+++ src/modules/portfolioSlider.js
@@ -5,11 +5,22 @@ const portfolioSlider = () =>{
     const portfolioPopup = document.querySelector('.popup-dialog-portfolio');
     const portfolioArrowLeft = document.getElementById('portfolio-arrow_left');
     const portfolioArrowRight = document.getElementById('portfolio-arrow_right');
+
+    const portfolioMobile = document.querySelector('.portfolio-slider-mobile')
+    const slidePortfolioMobile = portfolioMobile.querySelectorAll('.portfolio-slider__slide-frame');
+    let positionPortfolioMobile = 0;
+    const portfolioTotal = portfolio.querySelector('.slider-counter-content__total');
+    const portfolioCurrent = portfolio.querySelector('.slider-counter-content__current');
+    const portfolioMobileArrowLeft = document.getElementById('portfolio-arrow-mobile_left');
+    const portfolioMobileArrowRight = document.getElementById('portfolio-arrow-mobile_right');
+    portfolioMobileArrowLeft.style.display = 'none';
+    portfolioTotal.innerHTML = slidePortfolioMobile.length;
+
     portfolio.addEventListener('click', (event) => {
-        const target = event.target;
-        const slideWidth = slidePortfolio[0].getBoundingClientRect().width;
+        const target = event.target;       
 
         if (target.closest('#portfolio-arrow_right') !== null || target.closest('#portfolio-arrow_left') !== null) {
+            const slideWidth = slidePortfolio[0].getBoundingClientRect().width;
             if (target.closest('#portfolio-arrow_left') !== null) {
                 if (positionPortfolio > 0) {
                     positionPortfolio--;
@@ -34,6 +45,36 @@ const portfolioSlider = () =>{
             slidePortfolio.forEach((item) => {
                 item.style.transform = `translateX(-${positionPortfolio*slideWidth}px)`;
             });
+        }
+        
+        if (target.closest('#portfolio-arrow-mobile_right') !== null || target.closest('#portfolio-arrow-mobile_left') !== null) {
+            const slideHeight = slidePortfolioMobile[0].getBoundingClientRect().height;
+            if (target.closest('#portfolio-arrow-mobile_left') !== null) {
+                if (positionPortfolioMobile > 0) {
+                    positionPortfolioMobile--; 
+                    portfolioMobileArrowRight.style.display = 'block';
+                    if (positionPortfolioMobile === 0){
+                        portfolioMobileArrowLeft.style.display = 'none';                        
+                    }
+                }
+            }
+            if (target.closest('#portfolio-arrow-mobile_right') !== null) {
+                if (positionPortfolioMobile < slidePortfolioMobile.length-1) {
+                    positionPortfolioMobile++;
+                    portfolioMobileArrowLeft.style.display = 'block';
+                    if (positionPortfolioMobile === slidePortfolioMobile.length - 1){
+                        portfolioMobileArrowRight.style.display = 'none';
+                    }
+                }
+
+            }
+            portfolioCurrent.innerHTML = positionPortfolioMobile + 1;
+            slidePortfolioMobile.forEach((item) => {
+                item.style.transform = `translateY(-${
+              slideHeight * positionPortfolioMobile}px`;             
+             
+            });
+            
         }
     });
 
